1708f699f29f5cd4074d46d7e0726d714b78628e
ranmatan21 <ranmatan11@gmail.com>
2025-03-23 15:17:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# התקנת תלות בסיס
-apt-get update
-apt-get install -y wget unzip curl gnupg
+# יצירת ספרייה זמנית לכרום
+mkdir -p .chrome
+cd .chrome
 
-# התקנת Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+# הורדת Chromium (גרסה קומפקטית)
+wget https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/1210855/chrome-linux.zip
+unzip chrome-linux.zip
 
-# הרצת הסקריפט שלך
+# חזרה לתיקיית הפרויקט הראשית
+cd ..
+
+# הרצת הסקריפט עם הפנייה ל־Chromium המקומי
 python3 twitter_scraper.py
