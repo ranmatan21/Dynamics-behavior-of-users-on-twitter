102704780f94da703b24bd345135e5f1dde1c1f2
ranmatan21 <ranmatan11@gmail.com>
2025-03-23 15:00:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# עדכון והתקנת כרום
+# התקנת תלות בסיס
 apt-get update
-apt-get install -y wget unzip curl
+apt-get install -y wget unzip curl gnupg
+
+# התקנת Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
